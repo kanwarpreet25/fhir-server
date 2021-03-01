783f1e294093b78caf2d06bd12a659761acbe21d
Andrea <andrea@kreichgauer.com>
2021-03-01 18:21:31 +0100

    add sentry config options

--- src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/JpaRestfulServer.java
@@ -3,6 +3,8 @@ package ca.uhn.fhir.jpa.starter;
 import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.jpa.starter.dotBase.PlainSystemProviderR4;
 import ca.uhn.fhir.jpa.starter.dotBase.ResponseInterceptorExternalReference;
+import ca.uhn.fhir.rest.server.interceptor.LoggingInterceptor;
+import io.sentry.Sentry;
 import javax.servlet.ServletException;
 
 public class JpaRestfulServer extends BaseJpaRestfulServer {
@@ -15,6 +17,17 @@ public class JpaRestfulServer extends BaseJpaRestfulServer {
 
     registerProvider(new PlainSystemProviderR4());
     registerInterceptor(new ResponseInterceptorExternalReference());
+
+    Sentry.init(
+      options -> {
+        options.setDsn(System.getenv("SENTRY_DSN"));
+        options.setEnvironment(System.getenv("SENTRY_ENVIRONMENT"));
+        options.setServerName(HapiProperties.getServerName());
+        options.setTracesSampleRate(1.0);
+        options.setConnectionTimeoutMillis(10000);
+        options.setReadTimeoutMillis(10000);
+      }
+    );
 
     FhirContext ctx = getFhirContext();
     ctx.getParserOptions().setStripVersionsFromReferences(false);
