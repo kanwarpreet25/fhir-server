57ea486c64455145cc29fa3a9720dae24c025d38
Andrea <andrea@kreichgauer.com>
2021-03-28 23:38:20 +0200

    add entity accessLog

---
+++ src/main/java/ca/uhn/fhir/jpa/starter/dotBase/entity/AccessLog.java
@@ -0,0 +1,40 @@
+package ca.uhn.fhir.jpa.starter.dotBase.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+
+@Entity
+@Table(
+  name = "ACCESS_LOGGING",
+  uniqueConstraints = {},
+  indexes = {
+    @Index(name = "IDX_LOG_ID", columnList = "LOG_ID", unique = true),
+    @Index(name = "IDX_METHOD", columnList = "METHOD", unique = false),
+    @Index(name = "IDX_USERNAME", columnList = "USERNAME", unique = false),
+    @Index(name = "IDX_URL", columnList = "URL", unique = false),
+    @Index(name="IDX_TIMESTAMP", columnList = "TIMESTAMP", unique = false)
+  }
+)
+public class AccessLog {
+  @Id
+  @Column(name = "LOG_ID")
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  public int id;
+
+  @Column(name = "METHOD")
+  public String method;
+
+  @Column(name = "USERNAME")
+  public String username;
+
+  @Column(name = "URL")
+  public String url;
+
+  @Column(name= "TIMESTAMP")
+  public String timestamp;
+}
