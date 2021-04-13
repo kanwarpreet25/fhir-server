08f8617fa2a9378f833160484ac58f4a3383ca3b
Andrea <andrea@kreichgauer.com>
2021-04-13 19:36:55 +0200

    rm unused imports

--- src/main/java/ca/uhn/fhir/jpa/starter/dotBase/interceptors/AuthenticationInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/dotBase/interceptors/AuthenticationInterceptor.java
@@ -7,12 +7,6 @@ import ca.uhn.fhir.rest.api.RestOperationTypeEnum;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.server.servlet.ServletRequestDetails;
 import io.jsonwebtoken.Claims;
-import io.sentry.Sentry;
-import io.sentry.protocol.User;
-import org.hl7.fhir.r4.model.DomainResource;
-import org.hl7.fhir.r4.model.Extension;
-import org.hl7.fhir.r4.model.Meta;
-import org.hl7.fhir.r4.model.StringType;
 
 public class AuthenticationInterceptor {
   private static final org.slf4j.Logger OUR_LOG = org.slf4j.LoggerFactory.getLogger(
