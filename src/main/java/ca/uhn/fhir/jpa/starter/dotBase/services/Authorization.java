8bf714b6bff00db4b8fb5fefc0926e55cd922c8e
Andrea <andrea@kreichgauer.com>
2021-04-15 01:11:27 +0200

    add isDraft check for QuestionnaireRepsonse resources
    rm duplicate interceptor

--- src/main/java/ca/uhn/fhir/jpa/starter/dotBase/services/Authorization.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/dotBase/services/Authorization.java
@@ -10,13 +10,12 @@ import org.hl7.fhir.instance.model.api.IBaseResource;
 import org.hl7.fhir.r4.model.Coding;
 import org.hl7.fhir.r4.model.DomainResource;
 import org.hl7.fhir.r4.model.Procedure;
+import org.hl7.fhir.r4.model.QuestionnaireResponse;
 import org.hl7.fhir.r4.model.Procedure.ProcedureStatus;
 import org.hl7.fhir.r4.model.ResourceType;
 
 public class Authorization implements IConsentService {
-  private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(
-    Authorization.class
-  );
+  private static final org.slf4j.Logger ourLog = org.slf4j.LoggerFactory.getLogger(Authorization.class);
 
   private static final Set<String> DRAFT_RESOURCE_TYPES;
 
@@ -30,10 +29,7 @@ public class Authorization implements IConsentService {
    * Invoked once at the start of every request
    */
   @Override
-  public ConsentOutcome startOperation(
-    RequestDetails theRequestDetails,
-    IConsentContextServices theContextServices
-  ) {
+  public ConsentOutcome startOperation(RequestDetails theRequestDetails, IConsentContextServices theContextServices) {
     return ConsentOutcome.PROCEED;
   }
 
@@ -41,15 +37,11 @@ public class Authorization implements IConsentService {
    * Can a given resource be returned to the user?
    */
   @Override
-  public ConsentOutcome canSeeResource(
-    RequestDetails theRequestDetails,
-    IBaseResource theResource,
-    IConsentContextServices theContextServices
-  ) {
+  public ConsentOutcome canSeeResource(RequestDetails theRequestDetails, IBaseResource theResource,
+      IConsentContextServices theContextServices) {
     if (isDraftResource(theResource)) {
-      return isAuthorizedRequester(theRequestDetails, (DomainResource) theResource)
-        ? ConsentOutcome.AUTHORIZED
-        : ConsentOutcome.REJECT;
+      return isAuthorizedRequester(theRequestDetails, (DomainResource) theResource) ? ConsentOutcome.AUTHORIZED
+          : ConsentOutcome.REJECT;
     }
     return ConsentOutcome.AUTHORIZED;
   }
@@ -58,52 +50,43 @@ public class Authorization implements IConsentService {
    * Modify resources that are being shown to the user
    */
   @Override
-  public ConsentOutcome willSeeResource(
-    RequestDetails theRequestDetails,
-    IBaseResource theResource,
-    IConsentContextServices theContextServices
-  ) {
+  public ConsentOutcome willSeeResource(RequestDetails theRequestDetails, IBaseResource theResource,
+      IConsentContextServices theContextServices) {
     return ConsentOutcome.AUTHORIZED;
   }
 
-  // TODO: would be helpful, if all resources have meta.draft attribute
-  // Otherwise server has to request referenced resources (as for external
-  // resources)
-  //TODO: Currently only Procedure Drafts are blocked
   private boolean isDraftResource(IBaseResource theResource) {
     if (DRAFT_RESOURCE_TYPES.contains(theResource.fhirType())) {
-      if (theResource instanceof Procedure) {
-        Procedure procedure = (Procedure) theResource;
-        return procedure.getStatus() == ProcedureStatus.INPROGRESS;
-      }
+      return isDraft(theResource);
     }
     return false;
   }
 
-  private boolean isAuthorizedRequester(
-    RequestDetails theRequestDetails,
-    DomainResource theResource
-  ) {
+  private static boolean isDraft(IBaseResource theResource) {
+    if (theResource instanceof Procedure) {
+      Procedure procedure = (Procedure) theResource;
+      return procedure.getStatus() == ProcedureStatus.INPROGRESS;
+    }
+    if (theResource instanceof QuestionnaireResponse) {
+      QuestionnaireResponse questionnaireResponse = (QuestionnaireResponse) theResource;
+      return questionnaireResponse.getExtensionByUrl("https://simplifier.net/dot.base/draft-action")==null;
+    }
+    return false;
+  }
+
+  private boolean isAuthorizedRequester(RequestDetails theRequestDetails, DomainResource theResource) {
     String requestingUser = (String) theRequestDetails.getAttribute("_username");
     return requesterOwnsDraftResource(theResource, requestingUser);
   }
 
-  private static boolean requesterOwnsDraftResource(
-    DomainResource theResource,
-    String requestingUser
-  ) {
+  private static boolean requesterOwnsDraftResource(DomainResource theResource, String requestingUser) {
     if (hasMatchingUsernameTag(theResource, requestingUser) != null) {
       return true;
     }
     return false;
   }
 
-  private static Coding hasMatchingUsernameTag(
-    DomainResource theResource,
-    String requestingUser
-  ) {
-    return theResource
-      .getMeta()
-      .getTag("https://simplifier.net/dot.base/requesting-username", requestingUser);
+  private static Coding hasMatchingUsernameTag(DomainResource theResource, String requestingUser) {
+    return theResource.getMeta().getTag("https://simplifier.net/dot.base/requesting-username", requestingUser);
   }
 }
