ec99b53183692348efefb31bb6a562f653ef9ba7
Daniel <i@dast.xyz>
2021-06-14 00:33:55 +0200

    refactor authorization interceptor

--- src/main/java/ca/uhn/fhir/jpa/starter/dotBase/interceptors/UserAuthorizationIntercerptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/dotBase/interceptors/UserAuthorizationIntercerptor.java
@@ -1,63 +1,61 @@
 package ca.uhn.fhir.jpa.starter.dotBase.interceptors;
 
-import java.util.List;
 import ca.uhn.fhir.jpa.starter.dotBase.DotbaseProperties;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
 import ca.uhn.fhir.rest.server.interceptor.auth.AuthorizationInterceptor;
 import ca.uhn.fhir.rest.server.interceptor.auth.IAuthRule;
 import ca.uhn.fhir.rest.server.interceptor.auth.RuleBuilder;
+import java.util.List;
 
 /**
  * Determines whether the caller is admin, normal user or no user
- * and authorizes accordingly.
- * 
+ * and authorizes access accordingly.
+ *
  * @pre register after {@link AuthenticationInterceptor}
  */
 public class UserAuthorizationIntercerptor extends AuthorizationInterceptor {
-    private static final List<IAuthRule> ADMIN_RULES = new RuleBuilder()
-            .allowAll().build();
-    private static final List<IAuthRule> USER_RULES = new RuleBuilder()
-            .deny("Log acess forbidden for non-admin")
-                .operation()
-                .named("$logs")
-                .atAnyLevel()
-                .andAllowAllResponses()
-                .andThen()
-            .deny("Force-reindexing forbidden for non-admin")
-                .operation()
-                .named("$mark-all-resources-for-reindexing")
-                .atAnyLevel()
-                .andAllowAllResponses()
-                .andThen()
-            .deny("Deleting meta forbidden for non-admin")
-                .operation()
-                .named("$meta-delete")
-                .atAnyLevel()
-                .andAllowAllResponses()
-                .andThen()
-            .allowAll().build();
-    private static final List<IAuthRule> DENY_ALL_RULES = new RuleBuilder()
-            .denyAll().build();
-    
-    private String getUsername(RequestDetails theRequestDetails) {
-        return theRequestDetails.getAttribute("_username").toString();
-    }
+  private static final List<IAuthRule> ADMIN_RULES = new RuleBuilder().allowAll().build();
+  private static final List<IAuthRule> DENY_ALL_RULES = new RuleBuilder().denyAll().build();
+  private static final List<IAuthRule> USER_RULES = new RuleBuilder()
+    .deny("Log access forbidden for non-admin")
+    .operation()
+    .named("$logs")
+    .atAnyLevel()
+    .andAllowAllResponses()
+    .andThen()
+    .deny("Force-reindexing forbidden for non-admin")
+    .operation()
+    .named("$mark-all-resources-for-reindexing")
+    .atAnyLevel()
+    .andAllowAllResponses()
+    .andThen()
+    .deny("Deleting meta forbidden for non-admin")
+    .operation()
+    .named("$meta-delete")
+    .atAnyLevel()
+    .andAllowAllResponses()
+    .andThen()
+    .allowAll()
+    .build();
 
-    private boolean isUserAdmin(String username) {
-        return username == DotbaseProperties.get("Dotbase.AdminUserName");
-    }
+  private String getUsername(RequestDetails theRequestDetails) {
+    return theRequestDetails.getAttribute("_username").toString();
+  }
 
-    private boolean isUser(String username) {
-        return username != null;
-    }
+  private boolean isAdmin(String username) {
+    return username.equals(DotbaseProperties.get("Dotbase.AdminUserName"));
+  }
 
-    @Override
-    public List<IAuthRule> buildRuleList(RequestDetails theRequestDetails) {
-        String username = getUsername(theRequestDetails);
+  private boolean isUser(String username) {
+    return username != null;
+  }
 
-        if (isUserAdmin(username)) return ADMIN_RULES;
-        if (isUser(username)) return USER_RULES;
-        return DENY_ALL_RULES;
-    }
+  @Override
+  public List<IAuthRule> buildRuleList(RequestDetails theRequestDetails) {
+    String username = getUsername(theRequestDetails);
 
+    if (isAdmin(username)) return ADMIN_RULES;
+    if (isUser(username)) return USER_RULES;
+    return DENY_ALL_RULES;
+  }
 }
