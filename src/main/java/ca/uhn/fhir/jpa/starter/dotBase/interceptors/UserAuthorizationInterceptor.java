19c6b8fdafe93bc633dafdb29db06c4c56d25f32
Daniel <i@dast.xyz>
2021-06-17 23:29:01 +0200

    refactor UserAuthorizationInterceptor

--- src/main/java/ca/uhn/fhir/jpa/starter/dotBase/interceptors/UserAuthorizationInterceptor.java
+++ src/main/java/ca/uhn/fhir/jpa/starter/dotBase/interceptors/UserAuthorizationInterceptor.java
@@ -1,49 +1,53 @@
 package ca.uhn.fhir.jpa.starter.dotBase.interceptors;
 
+import ca.uhn.fhir.jpa.starter.HapiProperties;
 import ca.uhn.fhir.jpa.starter.dotBase.DotbaseProperties;
 import ca.uhn.fhir.rest.api.server.RequestDetails;
+import ca.uhn.fhir.rest.client.interceptor.LoggingInterceptor;
 import ca.uhn.fhir.rest.server.interceptor.auth.AuthorizationInterceptor;
 import ca.uhn.fhir.rest.server.interceptor.auth.IAuthRule;
 import ca.uhn.fhir.rest.server.interceptor.auth.RuleBuilder;
 import java.util.List;
 
 /**
- * Determines whether the caller is admin, normal user or no user
- * and authorizes access accordingly.
- *
- * @pre register after {@link AuthenticationInterceptor}
+ * Determines whether the caller is admin, normal user or no user and authorizes
+ * access accordingly.
  */
 public class UserAuthorizationInterceptor extends AuthorizationInterceptor {
+  private static final String[] RESTRICTED_ENDPOINTS = {
+    "$logs",
+    "$mark-all-resources-for-reindexing",
+    "$meta-delete"
+  };
+
   private static final List<IAuthRule> ADMIN_RULES = new RuleBuilder().allowAll().build();
   private static final List<IAuthRule> DENY_ALL_RULES = new RuleBuilder().denyAll().build();
-  private static final List<IAuthRule> USER_RULES = new RuleBuilder()
-    .deny("Log access forbidden for non-admin")
-    .operation()
-    .named("$logs")
-    .atAnyLevel()
-    .andAllowAllResponses()
-    .andThen()
-    .deny("Force-reindexing forbidden for non-admin")
-    .operation()
-    .named("$mark-all-resources-for-reindexing")
-    .atAnyLevel()
-    .andAllowAllResponses()
-    .andThen()
-    .deny("Deleting meta forbidden for non-admin")
-    .operation()
-    .named("$meta-delete")
-    .atAnyLevel()
-    .andAllowAllResponses()
-    .andThen()
-    .allowAll()
-    .build();
+  private static final List<IAuthRule> USER_RULES = buildUserRules();
+
+  private static List<IAuthRule> buildUserRules() {
+    RuleBuilder ruleBuilder = new RuleBuilder();
+    for (String endpoint : RESTRICTED_ENDPOINTS) ruleBuilder
+      .deny("Restricted Endpoint or Operation - " + endpoint)
+      .operation()
+      .named("$logs")
+      .atAnyLevel()
+      .andAllowAllResponses()
+      .andThen();
+    return ruleBuilder.allowAll().build();
+  }
 
   private String getUsername(RequestDetails theRequestDetails) {
-    return theRequestDetails.getAttribute("_username").toString();
+    String username = theRequestDetails.getAttribute("_username").toString();
+    if (username == null) throw new Error("Missing username");
+    return username;
   }
 
   private boolean isAdmin(String username) {
-    return username.equals(DotbaseProperties.get("Dotbase.AdminUserName"));
+    String adminUserName = DotbaseProperties.get("Dotbase.AdminUserName");
+    if (adminUserName == null || adminUserName.equals("")) System.out.println(
+      "WARN: No admin user set in dotbase.properties"
+    );
+    return username.equals(adminUserName);
   }
 
   private boolean isUser(String username) {
@@ -52,8 +56,9 @@ public class UserAuthorizationInterceptor extends AuthorizationInterceptor {
 
   @Override
   public List<IAuthRule> buildRuleList(RequestDetails theRequestDetails) {
-    String username = getUsername(theRequestDetails);
+    if (!HapiProperties.isAuthenticationInterceptorEnabled()) return ADMIN_RULES;
 
+    String username = getUsername(theRequestDetails);
     if (isAdmin(username)) return ADMIN_RULES;
     if (isUser(username)) return USER_RULES;
     return DENY_ALL_RULES;
